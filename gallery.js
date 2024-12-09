7088625b547d0da5fcc2bef40225beda5b1a2b16
FedericoCalzoni <federicocalzoni01@gmail.com>
2024-12-09 20:23:16 +0100

    fix: Initialize lightGallery only once after justifiedGallery completion. full screen capabilities and remove thumbnails in full screen

--- gallery.js
+++ gallery.js
@@ -6,23 +6,23 @@ document.addEventListener("DOMContentLoaded", function() {
         return;
     }
 
+    let lightGalleryInitialized = false;
+
     function calculateRowHeight() {
         var screenWidth = window.innerWidth;
         console.log("Screen width: " + screenWidth);
         var rowHeight;
     
         if (screenWidth < 450) {
-            rowHeight = 110; // Example value for small screens
+            rowHeight = 110; // small screens
         } else if (screenWidth < 800) {
-            rowHeight = 150; // Example value for medium screens
+            rowHeight = 150; // medium screens
         } else {
-            rowHeight = 200; // Example value for large screens
+            rowHeight = 200; // large screens
         }
     
         return rowHeight;
     }
-
-    
 
     console.log("Gallery element found");
 
@@ -42,39 +42,26 @@ document.addEventListener("DOMContentLoaded", function() {
     }).on("jg.complete", function () {
         console.log("justifiedGallery initialization complete");
 
-        // Check if lightGallery is defined
-        if (typeof lightGallery === 'undefined') {
-            console.error("lightGallery is not defined");
-            return;
+        if (!lightGalleryInitialized) {
+            // Initialize lightGallery after justifiedGallery is complete
+            lightGallery(galleryElement, {
+                plugins: [lgZoom, lgThumbnail, lgFullscreen],
+                speed: 500,
+                thumbnail: true,
+                animateThumb: true,
+                showZoomInOutIcons: true,
+                actualSize: false,
+                hideScrollbar: true,
+                mobileSettings: {
+                    controls: false,
+                    showCloseIcon: true,
+                    download: true,
+                    rotate: false,
+                    fullScreen: true
+                }
+            });
+            console.log("lightGallery initialization complete");
+            lightGalleryInitialized = true;
         }
-
-        // Initialize lightGallery after justifiedGallery is complete
-        lightGallery(galleryElement, {
-            plugins: [lgZoom, lgThumbnail],
-            speed: 500,
-            thumbnail: true,
-            animateThumb: true,
-            mobileSettings: {
-                controls: false,
-                showCloseIcon: true,
-                download: true,
-                rotate: false
-            }
-        });
-
-        console.log("lightGallery initialization complete");
     });
-
-    // Add passive event listeners for touch and wheel events
-    galleryElement.addEventListener('touchstart', function(e) {
-        // Your touchstart logic here
-    }, { passive: true });
-
-    galleryElement.addEventListener('touchmove', function(e) {
-        // Your touchmove logic here
-    }, { passive: true });
-
-    galleryElement.addEventListener('wheel', function(e) {
-        // Your wheel logic here
-    }, { passive: true });
 });
